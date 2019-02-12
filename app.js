1aad43d06d2b4b7e311a5cf138ad86bdb226174b
acrus865 <tannymann865@gmail.com>
2019-02-12 13:14:17 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,93 @@
+function setup() {
+  var amountInput = document.getElementById("amount");
+
+
+  function yesnoCheck() {
+   if (document.getElementById('memo').checked) {
+       document.getElementById('memo').style.display = 'block';
+   }
+   else {document.getElementById('memo').style.display = 'none';}
+}
+
+
+  // document.getElementById("memo").onclick =
+  //   function () {
+  //
+  //     if (memo.checked) {
+  //
+  //       memoStorage = document.getElementById("amount").value;
+  //
+  //       setLabel("Memo ");
+  //     }
+  //
+  //   };
+  // document.getElementById("priority").onclick =
+  //   function () {
+  //
+  //
+  //     if (priority.checked) {
+  //
+  //       priorityStorage = document.getElementById("amount").value;
+  //
+  //       setLabel("Priority ");
+  //     }
+  //
+  //   };
+  document.getElementById("enter").onclick =
+    function () {
+      var table = document.getElementById('tableOne');
+
+      var row = table.insertRow();
+
+      var cell1 = row.insertCell(rowNumber);
+      var cell2 = row.insertCell(memo);
+      var cell3 = row.insertCell(priority);
+
+      document.getElementById("memo").value = "";
+      document.getElementById("priority").value = "";
+    };
+
+
+
+
+}
+
+
+
+function setLabel(newLabel) {
+  var label = document.getElementById("inputLabel");
+  label.innerHTML = newLabel;
+}
+
+function storeMemo(input) {
+  var storage = input;
+}
+
+function storePriority(input) {
+  var storage = input;
+}
+
+// function store() {
+//   var memoText = memo.innerHTML;
+//   var priorityText = priority.innerHTML;
+//   console.log(memoText);
+//   console.log(priorityText);
+// }
+
+
+
+// function enter() {
+//   //takes memo and inserts in addToTable
+//   //takes priority and inserts into tableOne
+//   //calls addtable function
+//   var memo = document.getElementById("memo");
+//   var priority = document.getElementById("priority");
+//
+//   var table = document.getElementById('tableOne');
+//   var tableRows = {1};
+//
+//   addToTable(tablerows.length, memo, priority);
+// }
+
+//takes input from id memo and stores it in variable
+//takes input from id priority and stores it in variable
